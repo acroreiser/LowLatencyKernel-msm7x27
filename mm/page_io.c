cca4e2b2e9c80e66479117487adcf64be6d95709
Okeer <serhio.amadey@gmail.com>
2016-08-16 02:21:26 +0700

    mm: rate limit write-errors to swap device
        If device memory is low zram frequently fails to allocate memory
    so we can't store the page in zram. The swap subsystem then still continues
    to try to swap the pages to zram as there is no way to stop the
    swap system from swapping during such errors. This can cause massive dmesg
    logspam.
        With this commit we can prevent the kernel log from being flooded with
    these messages.
    
    Change-Id: Iee6a5cd4c4da52efdbd5c8954cc02c691f9b5b9e

--- mm/page_io.c
+++ mm/page_io.c
@@ -21,6 +21,7 @@
 #include <linux/aio.h>
 #include <linux/blkdev.h>
 #include <asm/pgtable.h>
+#include <linux/ratelimit.h>
 
 static struct bio *get_swap_bio(gfp_t gfp_flags,
 				struct page *page, bio_end_io_t end_io)
@@ -42,10 +43,13 @@ static struct bio *get_swap_bio(gfp_t gfp_flags,
 	return bio;
 }
 
+#define WRITE_ERROR_LOG_RATE_MS 1000
+
 static void end_swap_bio_write(struct bio *bio, int err)
 {
 	const int uptodate = test_bit(BIO_UPTODATE, &bio->bi_flags);
 	struct page *page = bio->bi_io_vec[0].bv_page;
+	static unsigned long swap_rs_time;
 
 	if (!uptodate) {
 		SetPageError(page);
@@ -58,10 +62,11 @@ static void end_swap_bio_write(struct bio *bio, int err)
 		 * Also clear PG_reclaim to avoid rotate_reclaimable_page()
 		 */
 		set_page_dirty(page);
-		printk(KERN_ALERT "Write-error on swap-device (%u:%u:%Lu)\n",
-				imajor(bio->bi_bdev->bd_inode),
-				iminor(bio->bi_bdev->bd_inode),
-				(unsigned long long)bio->bi_sector);
+		if (printk_timed_ratelimit(&swap_rs_time, WRITE_ERROR_LOG_RATE_MS))
+			printk(KERN_ALERT "Write-error on swap-device (%u:%u:%Lu)\n",
+					imajor(bio->bi_bdev->bd_inode),
+					iminor(bio->bi_bdev->bd_inode),
+					(unsigned long long)bio->bi_sector);
 		ClearPageReclaim(page);
 	}
 	end_page_writeback(page);
